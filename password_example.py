861990aa340effd8d8fd1413096852162fcc3992
Chenyuhan_Shen <chenyuhan.shen@my.jcu.edu.au>
2021-11-24 19:38:01 +0800

    Refactor checking of password into a function

--- password_example.py
+++ password_example.py
@@ -8,10 +8,14 @@ Password Checker. Created by Chenyuhan Shen. 24-11-2021
 def main():
     """Starting Program"""
     password = "12345678"
-    if password == "12345678":
+    if check_password(password):
         print("Logged in")
     else:
         print("Illegal access")
+
+
+def check_password(password):
+    return password == "12345678"
 
 
 main()
