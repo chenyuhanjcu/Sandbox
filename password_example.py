12a400baf650165f9c917d0e93da131caf442b90
Chenyuhan_Shen <chenyuhan.shen@my.jcu.edu.au>
2021-11-24 19:17:36 +0800

    Add password checking function

---
+++ password_example.py
@@ -0,0 +1,17 @@
+"""
+Check Login Credentials
+
+Password Checker. Created by Chenyuhan Shen. 24-11-2021
+"""
+
+
+def main():
+    """Starting Program"""
+    password = "12345678"
+    if password == "12345678":
+        print("Logged in")
+    else:
+        print("Illegal access")
+
+
+main()
